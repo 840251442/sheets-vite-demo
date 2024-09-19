0aec4153bf64280ef1e5c617c45241bb17e114d5
840251442 <840251442@qq.com>
2024-09-19 17:58:50 +0800

    当有两个合并单元格都设置了边框的时候 有一个会丢失

--- src/main.ts
+++ src/main.ts
@@ -48,99 +48,262 @@ univer.registerPlugin(UniverSheetsNumfmtPlugin);
 univer.registerPlugin(UniverSheetsFormulaPlugin);
 
 // create univer sheet instance
-univer.createUnit(UniverInstanceType.UNIVER_SHEET, {
-  "id": "gyI0JO",
-  "sheetOrder": [
-      "RSfWjJFv4opmE1JaiRj80"
-  ],
-  "name": "",
-  "appVersion": "0.1.11",
-  "locale": "zhCN",
-  "styles": {},
-  "sheets": {
-      "RSfWjJFv4opmE1JaiRj80": {
-          "id": "RSfWjJFv4opmE1JaiRj80",
-          "name": "测试",
-          "tabColor": "",
-          "hidden": 0,
-          "rowCount": 30,
-          "columnCount": 10,
-          "zoomRatio": 1,
-          "freeze": {
-              "startRow": -1,
-              "startColumn": -1,
-              "ySplit": 0,
-              "xSplit": 0
-          },
-          "scrollTop": 0,
-          "scrollLeft": 0,
-          "defaultColumnWidth": 73,
-          "defaultRowHeight": 23,
-          "mergeData": [],
-          "cellData": {},
-          "rowData": {},
-          "columnData": {
-              "0": {
-                  "w": 125,
-                  "hd": 0
-              },
-              "1": {
-                  "w": 125,
-                  "hd": 0
-              },
-              "2": {
-                  "w": 125,
-                  "hd": 0
-              },
-              "3": {
-                  "w": 125,
-                  "hd": 0
-              },
-              "4": {
-                  "w": 125,
-                  "hd": 0
-              },
-              "5": {
-                  "w": 125,
-                  "hd": 0
-              },
-              "6": {
-                  "w": 125,
-                  "hd": 0
-              },
-              "7": {
-                  "w": 125,
-                  "hd": 0
-              },
-              "8": {
-                  "w": 125,
-                  "hd": 0
-              },
-              "9": {
-                  "w": 125,
-                  "hd": 0
-              }
-          },
-          "showGridlines": 1,
-          "rowHeader": {
-              "width": 46,
-              "hidden": 0
-          },
-          "columnHeader": {
-              "height": 20,
-              "hidden": 0
-          },
-          "selections": [
-              "A1"
-          ],
-          "rightToLeft": 0
-      }
-  },
-  "resources": [
-      {
-          "name": "SHEET_DEFINED_NAME_PLUGIN",
-          "data": ""
-      }
-  ]
+const work: any = univer.createUnit(UniverInstanceType.UNIVER_SHEET, {
+    "id": "workbook-01",
+    "sheetOrder": [
+        "sheet-01",
+        "sheet-02",
+        "sheet-03"
+    ],
+    "name": "universheet",
+    "appVersion": "3.0.0-alpha",
+    "locale": "zhCN",
+    "styles": {
+        "ZBx-Mb": {
+            "bd": {
+                "t": null,
+                "b": null,
+                "r": null,
+                "l": null
+            }
+        },
+        "hcu9cZ": {
+            "bd": {
+                "t": {
+                    "s": 1,
+                    "cl": {
+                        "rgb": "#000000"
+                    }
+                },
+                "b": {
+                    "s": 1,
+                    "cl": {
+                        "rgb": "#000000"
+                    }
+                },
+                "l": {
+                    "s": 1,
+                    "cl": {
+                        "rgb": "#000000"
+                    }
+                }
+            }
+        },
+        "ZQGRB0": {
+            "bd": {
+                "t": {
+                    "s": 1,
+                    "cl": {
+                        "rgb": "#000000"
+                    }
+                },
+                "b": {
+                    "s": 1,
+                    "cl": {
+                        "rgb": "#000000"
+                    }
+                },
+                "r": {
+                    "s": 1,
+                    "cl": {
+                        "rgb": "#000000"
+                    }
+                }
+            }
+        },
+        "rFF4aY": {
+            "bd": {
+                "b": {
+                    "s": 1,
+                    "cl": {
+                        "rgb": "#000000"
+                    }
+                },
+                "l": {
+                    "s": 1,
+                    "cl": {
+                        "rgb": "#000000"
+                    }
+                }
+            }
+        }
+    },
+    "sheets": {
+        "sheet-01": {
+            "type": 0,
+            "id": "sheet-01",
+            "cellData": {
+                "0": {
+                    "0": {
+                        "s": "hcu9cZ"
+                    },
+                    "1": {
+                        "s": "ZQGRB0"
+                    },
+                    "2": {
+                        "s": "ZBx-Mb"
+                    }
+                },
+                "1": {
+                    "0": {
+                        "s": "ZBx-Mb"
+                    },
+                    "1": {
+                        "s": "rFF4aY"
+                    },
+                    "2": {
+                        "s": "ZQGRB0"
+                    },
+                    "3": {
+                        "s": "ZBx-Mb"
+                    }
+                },
+                "2": {
+                    "1": {
+                        "s": "ZBx-Mb"
+                    },
+                    "2": {
+                        "s": "ZBx-Mb"
+                    }
+                }
+            },
+            "name": "sheet1",
+            "tabColor": "red",
+            "hidden": 0,
+            "rowCount": 1000,
+            "columnCount": 20,
+            "zoomRatio": 1,
+            "scrollTop": 200,
+            "scrollLeft": 100,
+            "defaultColumnWidth": 93,
+            "defaultRowHeight": 27,
+            "status": 1,
+            "showGridlines": 1,
+            "hideRow": [],
+            "hideColumn": [],
+            "rowHeader": {
+                "width": 46,
+                "hidden": 0
+            },
+            "columnHeader": {
+                "height": 20,
+                "hidden": 0
+            },
+            "selections": [
+                "A2"
+            ],
+            "rightToLeft": 0,
+            "pluginMeta": {},
+            "freeze": {
+                "xSplit": 0,
+                "ySplit": 0,
+                "startRow": -1,
+                "startColumn": -1
+            },
+            "mergeData": [
+                {
+                    "startRow": 0,
+                    "startColumn": 0,
+                    "endRow": 0,
+                    "endColumn": 1,
+                    "rangeType": 0,
+                    "unitId": "workbook-01",
+                    "sheetId": "sheet-01"
+                },
+                {
+                    "startRow": 1,
+                    "startColumn": 1,
+                    "endRow": 1,
+                    "endColumn": 2,
+                    "rangeType": 0,
+                    "unitId": "workbook-01",
+                    "sheetId": "sheet-01"
+                }
+            ],
+            "rowData": {
+                "0": {
+                    "hd": 0,
+                    "h": 27
+                }
+            },
+            "columnData": {
+                "1": {
+                    "w": 93,
+                    "hd": 0
+                }
+            }
+        },
+        "sheet-02": {
+            "type": 0,
+            "id": "sheet-02",
+            "name": "sheet2",
+            "cellData": {},
+            "tabColor": "",
+            "hidden": 0,
+            "rowCount": 1000,
+            "columnCount": 20,
+            "zoomRatio": 1,
+            "freeze": {
+                "xSplit": 0,
+                "ySplit": 0,
+                "startRow": -1,
+                "startColumn": -1
+            },
+            "scrollTop": 0,
+            "scrollLeft": 0,
+            "defaultColumnWidth": 88,
+            "defaultRowHeight": 24,
+            "mergeData": [],
+            "rowData": {},
+            "columnData": {},
+            "showGridlines": 1,
+            "rowHeader": {
+                "width": 46,
+                "hidden": 0
+            },
+            "columnHeader": {
+                "height": 20,
+                "hidden": 0
+            },
+            "rightToLeft": 0
+        },
+        "sheet-03": {
+            "type": 0,
+            "id": "sheet-03",
+            "name": "sheet3",
+            "cellData": {},
+            "tabColor": "",
+            "hidden": 0,
+            "rowCount": 1000,
+            "columnCount": 20,
+            "zoomRatio": 1,
+            "freeze": {
+                "xSplit": 0,
+                "ySplit": 0,
+                "startRow": -1,
+                "startColumn": -1
+            },
+            "scrollTop": 0,
+            "scrollLeft": 0,
+            "defaultColumnWidth": 88,
+            "defaultRowHeight": 24,
+            "mergeData": [],
+            "rowData": {},
+            "columnData": {},
+            "showGridlines": 1,
+            "rowHeader": {
+                "width": 46,
+                "hidden": 0
+            },
+            "columnHeader": {
+                "height": 20,
+                "hidden": 0
+            },
+            "rightToLeft": 0
+        }
+    },
+    "resources": []
 }
 );
+
+console.log(work.save())
